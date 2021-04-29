069c907719de6824c448bf4bf69d591aefc625ee
SpecterNinjas <47240330+SpecterNinjas@users.noreply.github.com>
2021-04-29 14:16:22 +0500

    New features

--- hokim_qabuli/urls.py
+++ hokim_qabuli/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('panel/', include('panel.urls')),
     url('bot/', include(u'telegrambot.urls')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
